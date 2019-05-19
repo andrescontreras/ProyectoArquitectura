e7556a82cec9271a94d24f5d75479a5e15faad81
Andres Contreras <andres_c8@outlook.com>
2019-05-19 09:41:49 -0500

    estructura SOAP

---
+++ .NET_Negocio/SOAP_Financiera/WSMovimientos.svc.cs
@@ -0,0 +1,24 @@
+﻿using LogicaFinanciera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SOAP_Financiera
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WSMovimientos" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select WSMovimientos.svc or WSMovimientos.svc.cs at the Solution Explorer and start debugging.
+	public class WSMovimientos : IWSMovimientos
+	{
+		private FinancieraEntities db = new FinancieraEntities();
+		public List<Movimiento> DoWork()
+		{
+			var a = db.Movimiento.ToList();
+
+			return a;
+			
+	}
+	}
+}
---
+++ .NET_Negocio/SOAP_Financiera/WSUsuarios.svc.cs
@@ -0,0 +1,24 @@
+﻿using LogicaFinanciera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SOAP_Financiera
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WSUsuarios" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select WSUsuarios.svc or WSUsuarios.svc.cs at the Solution Explorer and start debugging.
+	public class WSUsuarios : IWSUsuarios
+	{
+		private FinancieraEntities db = new FinancieraEntities();
+		public List<Usuario> DoWork()
+		{
+			var a = db.Usuario.ToList();
+
+			return a;
+
+		}
+	}
+}
