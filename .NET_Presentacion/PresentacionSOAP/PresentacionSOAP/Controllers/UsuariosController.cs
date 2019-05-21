30feadb59a4b78398e966957a1a4091f26cbaa55
Andres Contreras <andres_c8@outlook.com>
2019-05-20 23:30:08 -0500

    Estructura Presentacion SOAP

---
+++ .NET_Presentacion/PresentacionSOAP/PresentacionSOAP/Controllers/UsuariosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentacionSOAP.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult List() {
+			ProxyUsuarios.WSUsuariosClient proxy = new ProxyUsuarios.WSUsuariosClient();
+			ProxyUsuarios.Usuario[] usuarios = proxy.GetAllUsuario();
+			return View(usuarios.ToList());
+		}
+
+		public ActionResult Edit() {
+			ProxyUsuarios.WSUsuariosClient proxy = new ProxyUsuarios.WSUsuariosClient();
+			ProxyUsuarios.Usuario[] usuarios = proxy.GetAllUsuario();
+			return View(usuarios[0]);
+		}
+
+		public ActionResult Details()
+		{
+			ProxyUsuarios.WSUsuariosClient proxy = new ProxyUsuarios.WSUsuariosClient();
+			ProxyUsuarios.Usuario[] usuarios = proxy.GetAllUsuario();
+			return View(usuarios[0]);
+		}
+	}
+}
