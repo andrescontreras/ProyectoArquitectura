c63a63bd805612846948bc6082e783d349db402a
ssalamanca <santiago.anime@hotmail.com>
2019-05-29 18:51:34 -0500

    Funciona enviar un email

--- JAVA/OPR/src/java/Negocio/PropiedadFacade.java
+++ JAVA/OPR/src/java/Negocio/PropiedadFacade.java
@@ -6,9 +6,11 @@
 package Negocio;
 
 import Integracion.IntegracionBD;
+import Integracion.IntegradorCorreo;
 import entities.Propiedad;
 import java.math.BigDecimal;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -34,13 +36,18 @@ public class PropiedadFacade extends AbstractFacade<Propiedad>{
     
     IntegracionBD bd  = new IntegracionBD();
     
+    @EJB
+    IntegradorCorreo correo; 
+    
     public void crearPropiedad(Propiedad propiedad){
-        bd.crearPropiedad(em, propiedad);
+        correo.enviarCorreo("prowebphp@gmail.com", "prowebphp@gmail.com", "php12345", "santiagosw18@gmail.com", "Propiedad agregada", "La propiedad que postulo fue agregada");
+        //bd.crearPropiedad(em, propiedad);
     }
     public Propiedad getPropiedad(Short id){
         return bd.findPropiedad(em, id);
     }
     public List<Propiedad> getPropiedades(){
+        
         return bd.getPropiedades(em);
     }
     public List<Propiedad> getPropiedadesxNombre(String nombre){
