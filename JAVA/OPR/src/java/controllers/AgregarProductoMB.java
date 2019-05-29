9a0e8986fedde49d453bb94afd40a3ecdeb91125
4Khaos <imroocker@gmail.com>
2019-05-29 13:47:46 -0500

    presentacionAgregarPropiedad (Post)

---
+++ JAVA/OPR/src/java/controllers/AgregarProductoMB.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import com.google.gson.Gson;
+import entities.Propiedad;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+/**
+ *
+ * @author imroo
+ */
+@Named(value = "agregarProductoMB")
+@Dependent
+public class AgregarProductoMB {
+
+    /**
+     * Creates a new instance of AgregarProductoMB
+     */
+    
+    private Propiedad propiedad = new Propiedad();
+    
+    public Propiedad getPropiedad() {
+        return propiedad;
+    }
+
+    public void setPropiedad(Propiedad propiedad) {
+        this.propiedad = propiedad;
+    }
+    
+    public void guardar() throws UnsupportedEncodingException {
+        System.out.println(propiedad.toString());
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            Gson gson = new Gson();
+            HttpPost request = new HttpPost("http://localhost:8080/OPR/webresources/entities.propiedad/");
+            StringEntity postingString = new StringEntity(gson.toJson(propiedad));
+            request.setEntity(postingString);
+            request.setHeader("Content-type","application/json");
+            HttpResponse result = httpClient.execute(request);
+            
+            String content = "N/A";
+            if (result.getEntity() != null)
+            {
+                content = EntityUtils.toString(result.getEntity());
+            }
+            
+            
+            int statusCode = result.getStatusLine().getStatusCode();
+            System.out.println("statusCode = " + statusCode);
+            System.out.println("content = " + content);
+        }
+        catch (IOException ex) {
+        }
+    }
+    
+    public AgregarProductoMB() {
+    }
+    
+}
